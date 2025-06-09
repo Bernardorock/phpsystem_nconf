80a4f3a26abb0ac5ffcd1feb934e9349240ba95d
Bernardorock <144393352+Bernardorock@users.noreply.github.com>
2025-06-09 14:14:05 -0300

    lista de produto

---
+++ view/listarProdutos.php
@@ -0,0 +1,21 @@
+<?php
+    include '../conect.php';
+
+    $sqlLista = 
+    "
+        select refsankhya, nomeproduto from produto
+        order by refsankhya
+    ";
+
+    $lista = $conect->prepare($sqlLista);
+    $lista->execute();
+    $captarLista = $lista->fetchAll();
+
+    foreach($captarLista as $valorLista)
+    {
+        echo '<ul>';
+        echo '<li>'.$valorLista[0]. ' . '.$valorLista[1].'</li>';
+        echo '</ul>';
+        echo '<br>';
+    }
+?>
